5a1853e3db9652f7c1f625db2893df42ab7d3e5f
Banou <me@banou.dev>
2023-10-06 20:40:44 +0200

    feat: ✨use the API_PORT as a setApiTarget target

--- src/shared-worker.ts
+++ src/shared-worker.ts
@@ -1,17 +1,46 @@
+import { makeCallListener, registerListener } from 'osra'
+
+import { getApiTarget, getApiTargetPort } from '@fkn/lib'
+
 import WorkerURL from './shared-worker/index?worker&url'
+
+export type Resolvers = typeof resolvers
 
 const worker = new SharedWorker(WorkerURL, { type: 'module' })
 
-worker.port.start()
+export const { resolvers } = registerListener({
+  resolvers: {
+    getApiTargetPort: makeCallListener(() => {
+      // console.log('RIPPLE getApiTargetPort RESOLVER')
+      return getApiTargetPort()
+    })
+  },
+  target: worker.port as unknown as Window,
+  key: 'shared-worker-fkn-api',
+  proxyTarget: await getApiTarget()
+})
+
+// console.log('getApiTargetPort calling')
+// getApiTargetPort().then(port => {
+//   console.log('-------------------port', port)
+//   // worker.port.postMessage(port)
+// })
 
 worker.port.addEventListener('error', (err) => {
   console.error(err)
 })
 
-worker.port.addEventListener('message', (event) => {
-  console.log('message', event.data)
-})
+// worker.port.addEventListener('message', (event) => {
+//   console.log('RIPPLE message', event.data)
+// })
 
-console.log('loading worker', worker)
+// console.log('loading worker', worker)
+
+// console.log('registerListener')
+
+// console.log('registerListener done', await getApiTarget())
+
+worker.port.start()
+// worker.port.postMessage()
 
 export default worker
--- src/shared-worker/index.ts
+++ src/shared-worker/index.ts
@@ -1,7 +1,12 @@
 import type { RxDocument } from 'rxdb'
 import type { TorrentDocument } from '../database'
 
-import { makeCallListener, registerListener } from 'osra'
+import { call, makeCallListener, registerListener } from 'osra'
+import { setApiTarget, torrent, serverProxyFetch } from '@fkn/lib'
+
+import type { Resolvers as SharedWorkerFknApiResolvers } from '../shared-worker'
+// import { makeCallListener, registerListener, setApiTarget } from 'osra'
+// import { torrent, serverProxyFetch } from '@fkn/lib'
 
 import { torrentCollection } from '../database'
 
@@ -18,19 +23,57 @@ const resolvers = {
 export type Resolvers = typeof resolvers
 
 
+// setTimeout(() => {
+//   console.log('setApiTarget', setApiTarget)
+// }, 2_000)
 
-globalThis.addEventListener('connect', ({ ports: [port] }) => {
-  console.log('port', port)
-  registerListener({
+let registers: ReturnType<typeof registerListener>
+
+globalThis.addEventListener('connect', (ev) => {
+  // console.log('CONNECT')
+  const { ports: [port] } = ev
+  port.start()
+
+
+  // console.log('RIPPLE SHARED WORKER CALL API_PORT', port)
+  call<SharedWorkerFknApiResolvers>(port, { key: 'shared-worker-fkn-api' })('getApiTargetPort')
+    .then(port => {
+      setApiTarget(port)
+    })
+
+  // try {
+  //   setApiTarget(port)
+  //   registerListener({
+  //     target: port as unknown as Worker,
+  //     resolvers
+  //   })
+  // } catch (err) {
+  //   console.error('err', err)
+  // }
+  // setApiTarget(port)
+  if (registers) registers.unregister()
+  registers = registerListener({
     target: port as unknown as Worker,
     resolvers
   })
+  // console.log('calling serverProxyFetch')
+  serverProxyFetch('https://example.com/')
+    .then(res => res.text())
+    .then(text => {
+      console.log('text', text)
+    })
+    .catch(err => {
+      console.error('err', err)
+    })
 })
 
-torrentCollection
-  .find()
-  .$
-  .subscribe(async (torrentDocuments: RxDocument<TorrentDocument>[]) => {
-    console.log('torrentDocuments', torrentDocuments)
-    
-  })
+// torrentCollection
+//   .find()
+//   .$
+//   .subscribe(async (torrentDocuments: RxDocument<TorrentDocument>[]) => {
+//     console.log('torrentDocuments', torrentDocuments)
+//     torrent({
+//       magnet: 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10',
+//       path: 'Sintel.mp4'
+//     })
+//   })
