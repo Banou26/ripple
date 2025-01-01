46820d0ffe66860f5c1d069a4e71e5cbfdb27370
Banou <me@banou.dev>
2025-01-01 02:57:35 +0100

    fix: 🐛remove 25mb/s download limit

--- src/webtorrent.ts
+++ src/webtorrent.ts
@@ -3,8 +3,4 @@ import _WebTorrent from 'webtorrent/dist/webtorrent.min.js'
 
 const WebTorrent = _WebTorrent as typeof WebTorrentType
 
-export const client =
-  new WebTorrent({
-    utp: false,
-    downloadLimit: 25_000_000
-  })
+export const client = new WebTorrent({ utp: false })
